481b46fcd5e4529daf01c92cd1d038e76c7f8811
Elliot Agro <elliotagro83@gmail.com>
2015-03-26 13:16:05 +0000

    Added String Calculator example

--- app/app.js
+++ app/app.js
@@ -1,5 +1,40 @@
 var App = function(){
 
-  this.test = "test string";
+    this.add = function(str){
+
+       if(str === ''){
+           return 0
+       }else{
+
+           var delim;
+           if(str[0] === '/' && str[1] === '/'){
+               delim = str[2];
+               str = str.substring(4);
+           }else{
+               delim = new RegExp('[,|\n]')
+           }
+
+           var numbers = str.split(delim);
+           var total = 0;
+           var error = 'is not a valid number';
+
+           for(var i = 0; i<numbers.length; i++){
+               if(Number(numbers[i]) < 0){
+                   error = numbers[i]+', ' + error;
+               }else{
+                   total += Number(numbers[i]);
+               }
+           }
+
+           if(error != 'is not a valid number'){
+               throw new Error(error);
+           }
+
+           return total;
+       }
+
+
+
+    };
 
 };
--- app/app.spec.js
+++ app/app.spec.js
@@ -1,16 +1,52 @@
 
-describe("My Kata", function() {
+describe('String Calculator', function() {
 
-  var app;
+    var app;
+    
+    beforeEach(function() {
+        app = new App();
+    });
+    
+    it('has an add method', function() {
+        expect(app.add).toBeDefined();
+    });
+    
+    it('should return 0 if passed an empty string ', function(){
+        expect(app.add('')).toBe(0);
+    });
 
-  beforeEach(function() {
-    app = new App();
-  });
+    it('should return 1 number', function(){
+        expect(app.add('3')).toBe(3);
+    });
 
-  it("contains spec with an expectation", function() {
+    it('should add 2 numbers together', function(){
+        expect(app.add('1,2')).toBe(3);
+    });
 
-    expect(app.test).toBe("test string");
+    it('should treat empty strings as 0', function(){
+        expect(app.add('2,')).toBe(2);
+    });
 
-  });
+    it('should add together more than two numbers', function(){
+        expect(app.add('1,2,3,4')).toBe(10);
+    });
+
+    it('should handle new lines as well as commas', function(){
+        expect(app.add('1\n2,3')).toBe(6);
+    });
+
+    it('should support the use of custom delimiters', function(){
+        expect(app.add('//%\n1%2%3')).toBe(6);
+    });
+
+    it('should throw exception on negative values', function(){
+
+        try{
+            app.add(null, '1,2,-3');
+        }catch(e){
+            expect(e).toBeDefined();
+        }
+
+    })
 
 });
